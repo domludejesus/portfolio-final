ad97e1ead1708a10cf51e7fc2adb96a8184b1ea0
domludejesus <domludejesus@gmail.com>
2023-05-28 15:02:05 -0400

    'skills section styles updated'

--- src/routes/sections/Skills.svelte
+++ src/routes/sections/Skills.svelte
@@ -5,27 +5,23 @@
         {
             id: 1,
             image: 'skills1.png',
-            title: 'Web Developer',
-            description: 'Description of Skill 1'
+            title: 'Front-end Development'
         },
         {
             id:2,
             image: '/skills2.png',
-            title: 'Ionic Developer',
-            description: 'Description of Skill 2'
+            title: 'UX Design'
         },
 
         {
             id:3,
             image: '/skills3.png',
-            title: 'Backend Developer',
-            description: 'Description of Skill 3'
+            title: 'Back-end Developer'
         },
         {
             id:4,
             image: '/skills4.png',
-            title: 'Content Creator',
-            description: 'Description of Skill 4'
+            title: 'Content Creator'
         },
     ];
 
@@ -41,18 +37,12 @@
      style="background-image: url('planet.jpg');">
     <h2 class="text-3xl font-bold mb-10">Skills</h2>
 
-    <div class="skills-content flex flex-wrap justify-around items-start w-full max-w-6xl"> <!-- Adjust the max-width as needed -->
-        {#each skills as skill, index (index)}
-            <div class="card w-15 glass mb-10"> <!-- Adjust the width and margin-bottom as needed -->
-                <figure>
-                    <img class="w-full h-48 object-cover" src="{skill.image}" alt="{skill.title}"/> <!-- Adjust the height as needed -->
-                </figure>
+    <div class="skills-content flex flex-wrap justify-center">
+        {#each skills as skill (skill.title)}
+        <div class="card w-34 sm:w-38 lg:w-40 glass mx-auto m-4 lg:m-8">
+                <figure><img src="{skill.image}" alt="{skill.title}"/></figure>
                 <div class="card-body">
-                    <h2 class="card-title text-2xl font-bold">{skill.title}</h2>
-                    <p>{skill.description}</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Learn more</button>
-                    </div>
+                    <h2 class="card-title">{skill.title}</h2>
                 </div>
             </div>
         {/each}
