e3b975ce02f02f534b70789c26fdb4035f26b40f
domludejesus <domludejesus@gmail.com>
2023-06-12 10:47:05 -0400

    'updated footer and testimonials'

--- src/routes/sections/Footer.svelte
+++ src/routes/sections/Footer.svelte
@@ -1,134 +1,50 @@
 <script>
     import { onMount, onDestroy } from 'svelte';
-    import * as THREE from 'three';
-
-    let footerContainer;
-    let footerCamera, footerScene, footerRenderer;
-    let footerGeometry, footerMaterial, footerMesh;
-    let footerOnWindowResize;
-    let footerAnimating = true;
-    let footerOnScroll
 
     let name = "";
     let email = "";
     let message = "";
+    let footerOnScroll;
 
-    function handleSubmit() {
-        console.log({ name, email, message });
-        // Add your form submission logic here
+    async function submitForm(e) {
+        e.preventDefault();
+
+        // your form data
+        const name = '';
+        const email = '';
+        const message = '';
+
+        const res = await fetch('/api/sendmail', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, message })
+        });
+
+        if (res.ok) {
+            alert('Email sent successfully');
+        } else {
+            const { error } = await res.json();
+            alert(error);
+        }
     }
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
 
-    onMount(async () => {
+    onMount(() => {
         if (typeof window !== 'undefined') {
-            // Create a Scene
-            footerScene = new THREE.Scene();
-
-            // Create a Camera
-            footerCamera = new THREE.PerspectiveCamera(
-                70,
-                window.innerWidth / window.innerHeight,
-                0.1,
-                1000
-            );
-            footerCamera.position.z = 3;
-
-            // Create a Geometry
-            footerGeometry = new THREE.SphereGeometry(1, 32, 16);
-
-            let texture = new THREE.TextureLoader().load('/city2.webp');
-
-            // Create a Material
-            footerMaterial = new THREE.MeshBasicMaterial({ map: texture });
-
-            // Create a Mesh containing the Geometry and Material
-            footerMesh = new THREE.Mesh(footerGeometry, footerMaterial);
-
-            // Scale the mesh
-            footerMesh.scale.set(1.7,1.7,1.7);
-
-            // Add the mesh to the Scene
-            footerScene.add(footerMesh);
-
-            // Create a Renderer
-            footerRenderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-
-            // Configure the Renderer
-            footerRenderer.setPixelRatio(window.devicePixelRatio);
-
-            // Append the renderer to the container
-            footerContainer.appendChild(footerRenderer.domElement);
-
-            // Debounced onResize handler
-            footerOnWindowResize = debounce(() => {
-                let rect = footerContainer.getBoundingClientRect();
-                // update the camera's aspect ratio and the renderer's size
-                footerCamera.aspect = rect.width / rect.height;
-                footerCamera.updateProjectionMatrix();
-                footerRenderer.setSize(rect.width, rect.height);
-            }, 250);
-            window.addEventListener('resize', footerOnWindowResize, false);
-
-            // Resize once on mount to make sure the model has the correct size
-            footerOnWindowResize();
-
-            // Create the animation
-            const animate = function () {
-                if (footerAnimating) {
-                    requestAnimationFrame(animate);
-                    footerMesh.rotation.x += 0.005;
-                    footerMesh.rotation.y += 0.005;
-                    footerRenderer.render(footerScene, footerCamera);
+            footerOnScroll = () => {
+                let scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop;
+                let footerSection = document.querySelector('.footer-section');
+                if(footerSection){
+                    footerSection.style.backgroundPositionY = (-scrollPos/4) + 'px';
                 }
             };
-
-            animate();
-
-            footerOnScroll = () => {
-            let scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop;
-            let footerSection = document.querySelector('.footer-section');
-            if(footerSection){
-                footerSection.style.backgroundPositionY = (-scrollPos/4) + 'px';
-            }
-        };
-        window.addEventListener('scroll', footerOnScroll);
-
-            // Intersection Observer to control the animation state based on visibility
-            const observer = new IntersectionObserver((entries) => {
-                if (entries[0].isIntersecting) {
-                    footerAnimating = true;
-                    animate();
-                } else {
-                    footerAnimating = false;
-                }
-            }, {
-                threshold: 0.1
-            });
-
-            // Start observing the container
-            observer.observe(footerContainer);
+            window.addEventListener('scroll', footerOnScroll);
         }
     });
 
     onDestroy(() => {
         if (typeof window !== 'undefined') {
-            window.removeEventListener('resize', footerOnWindowResize, false);
-            while (footerContainer.firstChild) {
-                footerContainer.firstChild.remove();
-            }
             window.removeEventListener('scroll', footerOnScroll);
         }
     });
@@ -141,7 +57,7 @@
         <div class="bg-secondary text-black p-4 md:p-8 rounded shadow-lg">
             <!-- Contact form goes here -->
             <div class="bg-white text-black p-4 md:p-8 rounded shadow-lg">
-                <form on:submit|preventDefault={handleSubmit}>
+                <form on:submit|preventDefault={submitForm}>
                     <div class="form-control">
                         <input class="input input-bordered" bind:value={name} type="text" placeholder="What's your name?" required>
                     </div>
@@ -158,11 +74,6 @@
             </div>
         </div>
     </div>
-    <div class="model-container md:w-1/2 w-full h-60 flex justify-center items-center md:mt-0"> 
-        <div class="model w-full h-60" bind:this={footerContainer}>
-            <!-- 3D model will be rendered here -->
-        </div>
-    </div>
 </div>
 
 <style>
@@ -171,14 +82,6 @@
             background-color: #f9a825;
             background-image: url('/planet.jpg');
             background-size: cover;
-        }
-
-        .model-container {
-            height: 150px;
-        }
-
-        .model {
-            height: 150px;
         }
 
         .contact-form {
--- src/routes/sections/Testimonials.svelte
+++ src/routes/sections/Testimonials.svelte
@@ -19,15 +19,15 @@
         {
             id: 3,
             image: '/person3.webp',
-            name: 'Person 2',
-            role: 'Role 2',
+            name: 'Person 3',
+            role: 'Role 3',
             testimonial: 'Great Attitude'
         },
         {
             id: 4,
             image: '/person4.webp',
-            name: 'Person 2',
-            role: 'Role 2',
+            name: 'Person 4',
+            role: 'Role 4',
             testimonial: 'Great Work Ethic'
         },
     ];
@@ -43,33 +43,21 @@
 <div class="testimonials-section bg-fixed bg-center bg-cover h-screen flex flex-wrap items-center justify-center px-8 py-20 lg:py-32 text-white" 
      style="background-image: url('/planet-2.webp');">
     <h2 class="w-full text-center text-5xl font-bold mb-8">Testimonials</h2>
-    <div class="carousel glass rounded-box w-full md:w-3/4 lg:w-1/3 whitespace-nowrap overflow-x-scroll py-8 px-4">
+    <div class="grid gap-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
         {#each testimonials as testimonial (testimonial.id)}
-            <div class="carousel-item w-full md:w-1/2 p-4 inline-block">
-                <div class="bg-base-content rounded-lg p-6 flex flex-col md:flex-row items-center md:items-start text-center md:text-left">
-                    <div class="avatar mb-4 md:mb-0 md:mr-6">
-                        <div class="w-24 h-24 rounded-full mx-auto md:mx-0">
-                            <img class="w-full h-full object-cover" src={testimonial.image} alt={testimonial.name} />
-                        </div>
+            <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{testimonial.name}</h3>
+                    <p class="my-4 font-light">"{testimonial.testimonial}"</p>
+                </blockquote>
+                <figcaption class="flex justify-center items-center space-x-3">
+                    <img class="w-9 h-9 rounded-full" src={testimonial.image} alt={testimonial.name}>
+                    <div class="space-y-0.5 font-medium dark:text-white text-left">
+                        <div>{testimonial.name}</div>
+                        <div class="text-sm font-light text-gray-500 dark:text-gray-400">{testimonial.role}</div>
                     </div>
-                    <div>
-                        <h3 class="text-xl font-bold mb-2">{testimonial.name}</h3>
-                        <hr class="mb-2"/>
-                        <p class="text-sm italic mb-4">{testimonial.role}</p>
-                        <p class="text-sm">"{testimonial.testimonial}"</p>
-                    </div>
-                </div>
-            </div>
+                </figcaption>
+            </figure>
         {/each}
     </div>
-    
 </div>
-
-<!-- <style>
-    @media (max-width: 600px) {
-        .testimonials-section {
-            height: auto;
-            background-image: url('/astronaut.jpg');
-        }
-    }
-</style> -->
